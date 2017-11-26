4d69042840a85f50a017ffc3c81d5b5464505b75
Volodymyr Huzar <vguzar@gmail.com>
2017-11-26 18:23:38 +0100

    Add CSV as accept type for the imported file Add new payers to resolve categories by payer

--- categoryResolver.js
+++ categoryResolver.js
@@ -1,25 +1,30 @@
-const groceries = ['CARREFOUR', 'ZABKA', 'GROSZEK', 'Piotr i Pawel', 'AUCHAN',
+const groceries = ['CARREFOUR', 'ZABKA', 'GROSZEK', 'Piotr i Pawel', 'AUCHAN', 'MARKET PUNKT',
 	'FRESHMARKET', 'KAUFLAND', 'TESCO', 'LIDL', 'BIEDRONKA', 'SNACK BAR SOUL', 'STARA PETLA']
 
 const desert = ['PAN AND CAKE', 'AWITEKS', 'PIEKARNIA']
 
-const lunch = 'Olimp', food = groceries.concat(desert).concat(lunch)
+const snack = ['McDonalds', 'ChipsyKing', 'SOUL']
+
+const lunch = 'Olimp', food = groceries.concat(desert, lunch, snack)
 
 const payerIncludesCategory = payer => category => payer.includes(category)
 
 const getFoodSubCategory = payer => {
 	if (desert.some(payerIncludesCategory(payer))) {
 		return 'Desert'
-	} if (payer.includes(lunch)) {
+	} else if (payer.includes(lunch)) {
 		return 'Lunch'
+	} else if (snack.some(payerIncludesCategory(payer))) {
+		return 'Snack'
 	}
 	return 'Groceries'
 }
 
-const entertainment = ['TRATTORIA', 'PIZZA', 'Sushi',
+const entertainment = ['TRATTORIA', 'PIZZA', 'Sushi', 'MONIKA SZCZYGIELSKA',
 	'Aleksandra Czaplicka', 'PHUONG DONG', 'DONG XUAN', 'DANIELE VANALI']
 
-const furniture = 'IKEA', tools = 'CASTORAMA', homeOffice = [furniture, tools]
+const furniture = 'IKEA', tools = ['CASTORAMA', 'ALIEXPRESS', 'KREDKA'],
+	homeOffice = tools.concat(furniture)
 
 const bank = ['Bank', 'INTEREST'];
 
@@ -32,7 +37,7 @@ const matchedBy = payer => categories => categories.some(payerIncludesCategory(p
 const getHomeOfficeSubCategory = payer => {
 	if (payer.includes(furniture)) {
 		return 'Furniture'
-	} 
+	}
 	return 'Tools'
 }
 
@@ -48,14 +53,18 @@ const getTransportSubCategory = payer => {
 const medical = ['APTEKA', 'SOFIMED'], cosmetic = 'ROSSMANN', healthCare = medical.concat(cosmetic)
 
 const getHealthCareSubCategory = payer => {
-    if (matchedBy(payer)(medical)) {
-        return 'Medical'
-    }
-    return 'Cosmetic'
+	if (matchedBy(payer)(medical)) {
+		return 'Medical'
+	}
+	return 'Cosmetic'
 }
 
+const documents = ['PRYZMAT']
+
+const getFamilySubCategory = () => 'Documents'
+
 exports.resolveCategory = payer => {
-    const payerMatchedBy = matchedBy(payer)
+	const payerMatchedBy = matchedBy(payer)
 	if (payerMatchedBy(food)) {
 		return getCategory('Food', getFoodSubCategory(payer))
 	} if (payerMatchedBy(transport)) {
@@ -70,6 +79,8 @@ exports.resolveCategory = payer => {
 		return getCategory('Bank', 'Commission')
 	} if (payerMatchedBy(drinks)) {
 		return getCategory('Drinks', 'Hot drinks')
+	} if (payerMatchedBy(documents)) {
+		return getCategory('Family', getFamilySubCategory(payer))
 	}
 	return { category: '', subCategory: '' }
 }

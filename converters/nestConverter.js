12a42bb991d0dee000eeafd968f6ee9e812a7f93
Volodymyr Huzar <vguzar@i.ua>
2023-01-22 19:30:58 +0100

    chore: upgrade dependencies
    Remove yarn from pipelines and use npm instead
    Upgade e2e pipeline to use native testcafe actions

--- converters/nestConverter.js
+++ converters/nestConverter.js
@@ -1,5 +1,5 @@
-const parseSync = require('csv-parse/lib/sync')
-const transform = require('stream-transform')
+const { parse } = require('csv-parse/sync')
+const { transform } = require('stream-transform')
 const moment = require('moment')
 const categoryResolver = require('../categoryResolver')
 const { sanitize } = require('../utils')
@@ -25,6 +25,6 @@ const getExpenseManagerRecord = recordCategoryResolver => record => {
 
 exports.convertCvsFileData = (input, categoriesMapping) => {
 	const getExpenseManagerWithMapping = getExpenseManagerRecord(categoryResolver.resolveCategory(categoriesMapping))
-	return transform(parseSync(input, { delimiter: ',', columns, relax_column_count: true }).slice(7).filter(record => record),
+	return transform(parse(input, { delimiter: ',', columns, relax_column_count: true }).slice(7).filter(record => record),
 		record => getExpenseManagerWithMapping(record))
 }

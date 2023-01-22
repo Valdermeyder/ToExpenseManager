164073e96467da46cf2032a1b1adb85de3f90fab
Volodymyr Huzar <vguzar@i.ua>
2023-01-22 18:42:04 +0100

    fix(pekao): parse thousands with spaces

--- converters/pekaoConverter.spec.js
+++ converters/pekaoConverter.spec.js
@@ -7,9 +7,9 @@ test("should be able to convert Pekao CSV files format", (done) => {
   };
   const input = `Data księgowania;Data waluty;Nadawca / Odbiorca;Adres nadawcy / odbiorcy;Rachunek źródłowy;Rachunek docelowy;Tytułem;Kwota operacji;Waluta;Numer referencyjny;Typ operacji;Kategoria
 19.04.2019;19.04.2019;H AND M GDANSK;;'25124012681111001119078209;;*********0085394;-3,20;PLN;'C992232109692387;TRANSAKCJA KARTĄ PŁATNICZĄ;Artykuły spożywcze
-19.04.2019;19.04.2019;Kasownik GDANSK;;'25124012681111001119078209;;*********0085394;-48,78;PLN;'C992231920047319;TRANSAKCJA KARTĄ PŁATNICZĄ;Artykuły spożywcze`;
+19.04.2019;19.04.2019;Kasownik GDANSK;;'25124012681111001119078209;;*********0085394;-4 008,78;PLN;'C992231920047319;TRANSAKCJA KARTĄ PŁATNICZĄ;Artykuły spożywcze`;
   const expected = `19.04.2019,-3.2,Personal,Clothing,Credit Card,,,H AND M GDANSK,,,Pekao
-19.04.2019,-48.78,Transport,Public transport,Credit Card,,,Kasownik GDANSK,,,Pekao
+19.04.2019,-4008.78,Transport,Public transport,Credit Card,,,Kasownik GDANSK,,,Pekao
 `;
   let transformedData = "";
 

be3077aa7277b5630c494e66ab0cefb758b15f6c
Volodymyr Huzar <vguzar@i.ua>
2023-02-12 18:10:33 +0100

    test: add e2e for monobank, santander and pekao

--- e2e/index.test.js
+++ e2e/index.test.js
@@ -24,16 +24,59 @@ async function waitForFile(filename) {
     }
 }
 
-const expectedFile = join(`${homedir()}`, 'Downloads', 'testData-converted.csv')
+let expectedFile
 
-test('should download converted csv without mapping', async t => {
+test('should download converted csv without mapping for City by default', async t => {
+    expectedFile = join(`${homedir()}`, 'Downloads', 'testDataCity-converted.csv')
     await t
-        .setFilesToUpload(Selector('#file'), 'testData.csv')
+        .setFilesToUpload(Selector('#file'), 'testDataCity.csv')
         .click(Selector('button'))
 
     await waitForFile(expectedFile)
     const file = readFileSync(expectedFile, 'utf-8')
 
     await t.expect(file).eql('30.10.2019,9839.29,Income,,Credit Card,,,Employer,,,CitiBank\n28.10.2019,-10,,,Credit Card,,,Play,,,CitiBank\n')
+})
+    .after(() => unlinkSync(expectedFile))
+
+test('should download converted csv without mapping for Monobank', async t => {
+    expectedFile = join(`${homedir()}`, 'Downloads', 'testDataMonobank-converted.csv')
+    await t
+        .setFilesToUpload(Selector('#file'), 'testDataMonobank.csv')
+        .click(Selector('#monobank'))
+        .click(Selector('button'))
+
+    await waitForFile(expectedFile)
+    const file = readFileSync(expectedFile, 'utf-8')
+
+    await t.expect(file).eql('30.10.2019,9839.29,Income,,Credit Card,,,Employer,,,Monobank\n28.10.2019,-10,,,Credit Card,,,Play,,,Monobank\n')
+})
+    .after(() => unlinkSync(expectedFile))
+
+test('should download converted csv without mapping for Pekao', async t => {
+    expectedFile = join(`${homedir()}`, 'Downloads', 'testDataPekao-converted.csv')
+    await t
+        .setFilesToUpload(Selector('#file'), 'testDataPekao.csv')
+        .click(Selector('#pekao'))
+        .click(Selector('button'))
+
+    await waitForFile(expectedFile)
+    const file = readFileSync(expectedFile, 'utf-8')
+
+    await t.expect(file).eql('30.10.2019,9839.29,Income,,Credit Card,,,Employer,,,Pekao\n28.10.2019,-10,,,Credit Card,,,Play,,,Pekao\n')
+})
+    .after(() => unlinkSync(expectedFile))
+
+test('should download converted csv without mapping for Santander', async t => {
+    expectedFile = join(`${homedir()}`, 'Downloads', 'testDataSantander-converted.csv')
+    await t
+        .setFilesToUpload(Selector('#file'), 'testDataSantander.csv')
+        .click(Selector('#santander'))
+        .click(Selector('button'))
+
+    await waitForFile(expectedFile)
+    const file = readFileSync(expectedFile, 'utf-8')
+
+    await t.expect(file).eql('30.10.2019,9839.29,Income,,Credit Card,,,Employer,,,Santander\n28.10.2019,-10,,,Credit Card,,,Play,,,Santander\n')
 })
     .after(() => unlinkSync(expectedFile))
